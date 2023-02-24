af55bdfed76636adab0d50c84be9f9804ea90923
Josh Farias <116445946+joshfarias@users.noreply.github.com>
2023-02-24 14:11:56 -0500

    Generates a 256x256 image using OpenAI's DALL-E image generator
    Generates a 256x256 image using OpenAI's DALL-E image generator

--- src/DALLEImageGenerator.py
+++ src/DALLEImageGenerator.py
@@ -19,14 +19,14 @@ import openai
 #read the api key from preset environment variable
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-#check if the api key is valid
+#check if the api key is valid, if not throw error
 if not openai.api_key:
     raise ValueError("No OpenAI API key provided. Please set the OPENAI_API_KEY environment variable.")
 
 #prompt the user for an image prompt
 prompt = input("Enter a prompt for DALL-E (Ex: 'a flying whale in outerspace 4k photorealistic'): ")
 
-#handles empty prompt
+#handle empty prompts
 if not prompt:
     print("Prompt cannot be empty. Please try again.")
     exit()
